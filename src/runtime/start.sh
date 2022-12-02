9fdbf9322f2fca15a16e8403e77664a3f8dd49d4
Dennis Meister <dennis.meister@bosch.com>
2022-12-02 15:11:41 +0100

    Fix runtime start script
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>

--- src/runtime/start.sh
+++ src/runtime/start.sh
@@ -4,5 +4,5 @@ echo "Hello, Runtime k3d!"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-${SCRIPT_DIR}/build_vehicleapp.sh | tee /dev/tty0
-${SCRIPT_DIR}/deploy_vehicleapp.sh | tee /dev/tty0
+${SCRIPT_DIR}/configure_controlplane.sh | tee /dev/tty0
+${SCRIPT_DIR}/deploy_runtime.sh | tee /dev/tty0
